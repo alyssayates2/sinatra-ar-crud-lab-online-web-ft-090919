8d27905461bc5e3db36f8c83b0a0297ca930398b
Alyssa Yates <bosslys@icloud.com>
2019-11-05 18:26:29 +0000

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,9 @@ class ArticlesController < ApplicationController
     erb :"articles/index"
   end
 
+  get '/articles/new' do
+    @article = Article.create(params[:title], params[:content])
+    erb :"articles/new"
+  end
+
 end
